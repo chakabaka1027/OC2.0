f5f96fcb5db6017419d92550b1001be084e79df0
chakabaka1027 <alexanderdcha@gmail.com>
2017-06-21 18:32:20 -0400

    added game

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public LayerMask blocks;
+
+	float distance = 10;
+
+	GameObject currentBlock;
+
+	bool mouseDown = false;
+
+//	// Update is called once per frame
+	void Update () {
+
+		//detect mouse input
+		if(Input.GetMouseButton(0)){
+			IdentifyBlock();
+			mouseDown = true;
+		} else {
+			mouseDown = false;
+			currentBlock = null;
+		}
+
+		//if you have selected a block
+		if (currentBlock != null){
+			DragBlock();
+		}
+
+	}
+
+	void IdentifyBlock(){
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+
+		if(Physics.Raycast(ray, out hit, Mathf.Infinity, blocks) && !mouseDown){
+			currentBlock = hit.collider.gameObject;
+		}
+
+	}
+
+	void DragBlock(){
+
+		currentBlock.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
+		
+	}
+
+
+}
