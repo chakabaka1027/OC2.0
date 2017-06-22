7ddcfb6465c1a003ce3032f1d43dfc7dbcc0f64c
chakabaka1027 <alexanderdcha@gmail.com>
2017-06-22 14:22:18 -0400

    block snap in place, not correct yet

---
+++ Assets/Snap.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snap : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 currentPos = transform.position;
+		transform.position = new Vector3(Mathf.Round(currentPos.x), Mathf.Round(currentPos.y), Mathf.Round(currentPos.z));	
+	}
+}
